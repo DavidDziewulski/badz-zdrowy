0efa8412ebb8454694389386c245bc58e6f8d4ef
Dawid Dziewulski <dawid.dziewulski@rad4m.com>
2022-12-12 17:34:48 +0100

    Add to less

--- build.ts
+++ build.ts
@@ -1,9 +1,26 @@
+import * as path from 'path';
 import { build } from 'esbuild';
+import EsbuildStyle from 'esbuild-style-plugin';
+import LessGlobImporter from 'less-plugin-glob';
 
 build({
+	incremental: true,
 	entryPoints: ['./src/frontend/index.tsx'],
 	bundle: true,
+	minify: true,
 	outfile: './public/main.js',
-	watch: true,
+	watch: {
+		onRebuild: () => {
+			console.log('🔧[frontend]: Builded!');
+		},
+	},
 	sourcemap: 'both',
-}).catch(() => process.exit(1))
+	plugins: [EsbuildStyle({
+		renderOptions: {
+			lessOptions: {
+				plugins: [LessGlobImporter],
+				paths: [path.resolve(__dirname, './src/frontend')],
+			},
+		},
+	})],
+}).catch(() => process.exit(1));
--- src/frontend/components/Error/ErrorMessage.tsx
+++ src/frontend/components/Error/ErrorMessage.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 export const ErrorMessage = ({ message }: Props) => (
-	<p className="mt-2 text-pink-600 text-sm">
+	<p data-role="error">
 		{message}
 	</p>
 );
